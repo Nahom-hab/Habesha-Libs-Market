98ef0582a660991957ad66ec2359394b92faee32
Nahom-hab <nahomhabtamu147@gmail.com>
2024-09-06 15:53:02 +0300

    fixed language issue

--- backend/frontend/src/zustand/useAdmin.js
+++ backend/frontend/src/zustand/useAdmin.js
@@ -1,8 +1,16 @@
 import { create } from 'zustand';
 
-const useAdmin = create((set) => ({
-    admin: JSON.parse(localStorage.getItem("Admin")),
-    setAdmin: (admin) => set({ admin }),
-}))
+const useAdmin = create((set) => {
+    // Get `isEng` value from localStorage or default to true if not foundnsn
+    const storedIsEng = JSON.parse(localStorage.getItem("isEng"));
+    const initialIsEng = storedIsEng !== null ? storedIsEng : true;
+
+    return {
+        admin: JSON.parse(localStorage.getItem("Admin")),
+        setAdmin: (admin) => set({ admin }),
+        isEng: initialIsEng,
+        setIsEng: (isEng) => set({ isEng }),
+    };
+});
 
 export default useAdmin;
