d99bc96ad73ae52f8f396b1f71f00f5c511a4c39
Nahom-hab <nahomhabtamu147@gmail.com>
2024-09-07 07:45:18 +0300

    fixed styling

--- backend/frontend/src/components/AdminNavigation.jsx
+++ backend/frontend/src/components/AdminNavigation.jsx
@@ -15,11 +15,11 @@ export default function AdminNavigation() {
     return (
         <div className="lg:h-screen lg:w-44 border-r border-slate-300  flex justify-evenly lg:justify-normal lg:flex-col lg:fixed">
             <div className='font-bold hidden lg:block   text-2xl pt-4'>Teke Habesha</div>
-            <nav className="flex lg:flex-col gap-4 p-6 lg:p-8">
+            <nav className="flex lg:flex-col gap-4 p-5 lg:p-8">
                 <NavLink
                     to="/admin"
                     className={({ isActive }) =>
-                        `text-base font-medium transition-colors duration-300 ${isActive ? "text-blue-600 font-semibold" : "text-gray-800 hover:text-blue-500"
+                        `text-base font-medium transition-colors duration-300 ${isActive ? "text-yellow-600 font-semibold" : "text-gray-800 hover:text-yellow-600"
                         }`
                     }
                 >
@@ -28,7 +28,7 @@ export default function AdminNavigation() {
                 <NavLink
                     to="/addproduct"
                     className={({ isActive }) =>
-                        `text-base font-medium transition-colors duration-300 ${isActive ? "text-blue-600 font-semibold" : "text-gray-800 hover:text-blue-500"
+                        `text-base font-medium transition-colors duration-300 ${isActive ? "text-yellow-600 font-semibold" : "text-gray-800 hover:text-yellow-600"
                         }`
                     }
                 >
@@ -37,13 +37,13 @@ export default function AdminNavigation() {
                 <NavLink
                     to="/editElements"
                     className={({ isActive }) =>
-                        `text-base font-medium transition-colors duration-300 ${isActive ? "text-blue-600 font-semibold" : "text-gray-800 hover:text-blue-500"
+                        `text-base font-medium transition-colors duration-300 ${isActive ? "text-yellow-600 font-semibold" : "text-gray-800 hover:text-yellow-600"
                         }`
                     }
                 >
                     Edit Elements
                 </NavLink>
-                <div className='lg:mt-32 lg:ml-0 ml-10'>
+                <div className='lg:mt-32 lg:ml-0 ml-6'>
                     <select
                         className='lg:block bg-[#FFF4E9] border-none outline-none'
                         value={isEng ? 'English' : 'Amharic'}
